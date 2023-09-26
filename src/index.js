6606d70387ac58f213049e0715de174e46ddf456
Hanna Dziakava <anne.dyakova@gmail.com>
2023-09-26 16:30:15 +0200

    solution for the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let newMatrix = [];
+
+    if (matrix) {
+        matrix.forEach((array, index) => {
+            if (index % 2 !== 0) {
+                newMatrix = [...newMatrix, ...array.reverse()];
+                return;
+            }
+            newMatrix = [...newMatrix, ...array];
+        });
+        return newMatrix;
+    } else {
+        return newMatrix;
+    }
+};
